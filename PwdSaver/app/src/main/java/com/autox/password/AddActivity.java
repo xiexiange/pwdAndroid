c34c8e28959f1f71b99c2636063b277ec6b444f7
echo.xie <echo.xie@cootek.cn>
2020-02-27 20:03:41 +0800

    [feature]密码只允许设置大小写数字和特殊符号

--- PwdSaver/app/src/main/java/com/autox/password/AddActivity.java
+++ PwdSaver/app/src/main/java/com/autox/password/AddActivity.java
@@ -3,8 +3,14 @@ package com.autox.password;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.text.method.DigitsKeyListener;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -30,7 +36,11 @@ public class AddActivity extends AppCompatActivity {
     private TYPE mType;
     private ImageView mIconIV;
     private TextView mTitleTV;
-
+    private EditText mAccountET;
+    private ImageView mAccountCloseIV;
+    private EditText mPwdET;
+    private ImageView mPwdCloseIV;
+    String mContentRegular = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
     private RelativeLayout mBackRL;
     private TextView mSaveTV;
     @Override
@@ -54,6 +64,10 @@ public class AddActivity extends AppCompatActivity {
         mTitleTV = findViewById(R.id.add_page_detail_title);
         mBackRL = findViewById(R.id.add_page_back_wrapper);
         mSaveTV = findViewById(R.id.add_page_save_btn);
+        mAccountET = findViewById(R.id.add_page_account_content);
+        mAccountCloseIV = findViewById(R.id.add_page_clear_account);
+        mPwdET = findViewById(R.id.add_page_pwd_content);
+        mPwdCloseIV = findViewById(R.id.add_page_clear_pwd);
         String title;
         int imageId;
         switch (mType) {
@@ -105,6 +119,88 @@ public class AddActivity extends AppCompatActivity {
                 Toast.makeText(AddActivity.this, "保存", Toast.LENGTH_SHORT).show();
             }
         });
+        mAccountCloseIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAccountET.setText("");
+                mAccountET.requestFocus();
+                InputMethodManager manager = ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE));
+                if (manager != null) manager.showSoftInput(v, 0);
+            }
+        });
+        mAccountET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int length = mAccountET.getText().toString().length();
+                if (length == 0) {
+                    mAccountCloseIV.setVisibility(View.GONE);
+                } else {
+                    mAccountCloseIV.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        mPwdCloseIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPwdET.setText("");
+                mPwdET.requestFocus();
+                InputMethodManager manager = ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE));
+                if (manager != null) manager.showSoftInput(v, 0);
+
+            }
+        });
+        mPwdET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable edt) {
+                String temp = edt.toString();
+                if(edt.toString().getBytes().length != edt.length()){
+                    edt.delete(temp.length()-1, temp.length());
+                }
+                try {
+                    temp = edt.toString();
+                    String tem = temp.substring(temp.length()-1, temp.length());
+                    char[] temC = tem.toCharArray();
+                    int mid = temC[0];
+                    boolean needDelete = true;
+                    if(mid>=33&&mid<=122){
+                        needDelete = false;
+                    }
+                    if (needDelete) {
+                        edt.delete(temp.length() - 1, temp.length());
+                    }
+
+                } catch (Exception e) {
+                }
+
+
+                int length = mPwdET.getText().toString().length();
+                if (length == 0) {
+                    mPwdCloseIV.setVisibility(View.GONE);
+                } else {
+                    mPwdCloseIV.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
     }
 
 }
