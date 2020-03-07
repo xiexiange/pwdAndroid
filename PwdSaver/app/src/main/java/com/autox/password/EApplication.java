257b55b7b4a7761b5376ab785338f117441645a5
echo.xie <echo.xie@cootek.cn>
2020-03-07 15:05:35 +0800

    [feature]集成bugly

--- PwdSaver/app/src/main/java/com/autox/password/EApplication.java
+++ PwdSaver/app/src/main/java/com/autox/password/EApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 import com.autox.password.localdata.sharedprefs.SharedPrefUtils;
+import com.tencent.bugly.crashreport.CrashReport;
 
 public class EApplication extends Application {
     private static Context mContext;
@@ -13,6 +14,7 @@ public class EApplication extends Application {
         super.onCreate();
         mContext = this;
         SharedPrefUtils.getInstance().init(this);
+        CrashReport.initCrashReport(getApplicationContext(), "b57fe81771", BuildConfig.DEBUG);
     }
 
     public static Context getContext() {
