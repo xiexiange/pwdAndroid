bdf083ff3146f18bd2fd1313a6b499be30b739f5
echo.xie <echo.xie@cootek.cn>
2020-03-18 08:42:04 +0800

    [feature]添加"我的"tab回来，新增登录和删除的逻辑

--- PwdSaver/app/src/main/java/com/autox/password/frames/MeFrameLayout.java
+++ PwdSaver/app/src/main/java/com/autox/password/frames/MeFrameLayout.java
@@ -7,17 +7,45 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 import com.autox.password.R;
+import com.autox.password.views.ItemViewList;
 
 public class MeFrameLayout extends Fragment
 {
     private View mRoot;
+    private ItemViewList mLoginView;
+    private ItemViewList mClearView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mRoot = inflater.inflate(R.layout.frame_me, null);
+        mLoginView = mRoot.findViewById(R.id.item_login);
+        mClearView = mRoot.findViewById(R.id.item_clear);
         return mRoot;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bindEvents();
+    }
+
+    private void bindEvents() {
+        mLoginView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "要登录", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mClearView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "已删除", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
