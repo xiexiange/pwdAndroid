02ec5efdfda3762efe1afc521486255e9bdab70f
echo.xie <echo.xie@cootek.cn>
2020-03-19 19:05:54 +0800

    [feature]添加备份入口

--- PwdSaver/app/src/main/java/com/autox/password/frames/MeFrameLayout.java
+++ PwdSaver/app/src/main/java/com/autox/password/frames/MeFrameLayout.java
@@ -25,14 +25,14 @@ import java.util.List;
 public class MeFrameLayout extends Fragment
 {
     private View mRoot;
-    private ItemViewList mLoginView;
+    private ItemViewList mBackupView;
     private ItemViewList mClearView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mRoot = inflater.inflate(R.layout.frame_me, null);
-        mLoginView = mRoot.findViewById(R.id.item_login);
+        mBackupView = mRoot.findViewById(R.id.item_backup);
         mClearView = mRoot.findViewById(R.id.item_clear);
         return mRoot;
     }
@@ -44,10 +44,10 @@ public class MeFrameLayout extends Fragment
     }
 
     private void bindEvents() {
-        mLoginView.setOnClickListener(new View.OnClickListener() {
+        mBackupView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "要登录", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "要备份", Toast.LENGTH_SHORT).show();
             }
         });
         mClearView.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +65,12 @@ public class MeFrameLayout extends Fragment
                                 EventBus.getDefault().post(new DbChanged());
                                 Toast.makeText(getActivity(), "已删除所有数据", Toast.LENGTH_SHORT).show();
                                 // todo 可以做个意见反馈
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
                             }
                         });
                 builder.create().show();
